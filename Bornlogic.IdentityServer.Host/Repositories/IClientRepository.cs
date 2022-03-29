0c52fed3f0833517411cc310ac3f957cc22248c3
VictorFZ <victorzec@hotmail.com>
2022-03-29 09:48:48 -0300

    insert + update

--- Bornlogic.IdentityServer.Host/Repositories/IClientRepository.cs
+++ Bornlogic.IdentityServer.Host/Repositories/IClientRepository.cs
@@ -5,5 +5,7 @@ namespace Bornlogic.IdentityServer.Host.Repositories
     public interface IClientRepository
     {
         Task<Client> GetByID(string id);
+        Task Update(Client client);
+        Task Insert(Client client);
     }
 }
