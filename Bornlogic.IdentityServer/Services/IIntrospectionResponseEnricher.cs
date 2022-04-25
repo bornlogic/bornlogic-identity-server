1b1ad8724f3ae06b8ef7aa58155e7cd0f54f0f0c
VictorFZ <victorzec@hotmail.com>
2022-04-25 16:36:54 -0300

    introspection enricher

---
+++ Bornlogic.IdentityServer/Services/IIntrospectionResponseEnricher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bornlogic.IdentityServer.Validation.Models;
+
+namespace Bornlogic.IdentityServer.Services
+{
+    public interface IIntrospectionResponseEnricher
+    {
+        Task Enrich(IDictionary<string, object> response, IntrospectionRequestValidationResult validationResult);
+    }
+}
