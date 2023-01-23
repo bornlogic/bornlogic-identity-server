391957f161165bea4ff05c12b48ee23a9ac95841
VictorFZ <victorzec@hotmail.com>
2023-01-23 11:28:57 -0300

    removed fixed code

--- Bornlogic.IdentityServer/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
+++ Bornlogic.IdentityServer/ResponseHandling/Default/AuthorizeInteractionResponseGenerator.cs
@@ -351,9 +351,7 @@ namespace Bornlogic.IdentityServer.ResponseHandling.Default
         {
             if (request == null)
                 throw new ArgumentNullException(nameof(request));
-
-            //request.Client.RequiresBusinessSelection = true;
-
+            
             var requiresBusinessSelect = request.Client.RequiresBusinessSelection && string.IsNullOrEmpty(businessSelect?.BusinessId) && string.IsNullOrEmpty(businessSelect?.UserBusinessScopedId);
 
             return new InteractionResponse { IsBusinessSelect = requiresBusinessSelect };
